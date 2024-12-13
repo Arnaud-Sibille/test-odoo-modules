0f2063539f042d051f1ccd39e970c197d41425c5
Arnaud Sibille <arsi@odoo.com>
2024-12-13 10:09:17 +0100

    Added test_module skeleton

---
+++ test_module/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Test module",
+    "description": "For testing purpose",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/test_model_views.xml',
+    ],
+}
