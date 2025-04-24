ab915461c113f4f5c3c7ea2c8325867aa9bd7399
Arnaud Sibille <arsi@odoo.com>
2025-04-24 18:36:38 +0200

    Add test_action_report

---
+++ test_action_report/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Test Action Report",
+    "description": "Testing Report Actions",
+    "author": "Arnaud S.",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    'data': [
+        'views/views.xml',
+    ],
+}
